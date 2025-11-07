9fb0ee0cc4266b6f9d4d2298c6b0052f3288170d
Summer Vogel <tsu.v@icloud.com>
2025-11-07 09:59:56 -0800

    dasda

--- yuribot/config.py
+++ yuribot/config.py
@@ -10,5 +10,5 @@ os.makedirs(DATA_DIR, exist_ok=True)
 TZ_NAME = os.getenv("TZ", "UTC")
 TZ = tz.gettz(TZ_NAME)
 
-BOT_DB_PATH = os.getenv('BOT_DB_PATH', os.path.join(DATA_DIR, 'bot.sqlite3')))
+BOT_DB_PATH = os.getenv('BOT_DB_PATH', os.path.join(DATA_DIR, 'bot.sqlite3'))
 DB_PATH = BOT_DB_PATH  # alias for backward-compat
