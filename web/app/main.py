e446200fe0554626b0a7fc198af323345156d148
Summer Vogel <tsu.v@icloud.com>
2025-11-11 12:30:49 -0800

    re-add security headers

--- web/app/main.py
+++ web/app/main.py
@@ -35,7 +35,7 @@ CSP = (
 class SecurityHeaders(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         resp = await call_next(request)
-        #resp.headers["Content-Security-Policy"] = CSP
+        resp.headers["Content-Security-Policy"] = CSP
         return resp
 
 
